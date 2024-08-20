90b246169e19cb41f5eae3c1d2e35277495bb435
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-08-20 08:41:50 +0530

    modularized at package lavel

---
+++ Lab core java sem 5/lab1/src/MyPackage1/MyDemo.java
@@ -0,0 +1,10 @@
+package MyPackage1;
+
+import MyMathematics.MyIntegerMath;
+
+public class MyDemo {
+	public static void main(String[] args) {
+		System.out.println(MyIntegerMath.factorial(5));
+		System.out.println(MyIntegerMath.isStrong(145));
+	}
+}
