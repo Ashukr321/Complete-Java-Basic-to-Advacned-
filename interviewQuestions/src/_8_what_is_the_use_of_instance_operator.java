966a687cddf92e482af728b4d3eeda6b1b7ec702
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-08-30 06:59:57 +0530

    use of instanceof operator

---
+++ interviewQuestions/src/_8_what_is_the_use_of_instance_operator.java
@@ -0,0 +1,6 @@
+public class _8_what_is_the_use_of_instance_operator {
+	public static void main(String[] args) {
+		String str = "Ashutosh kumar";
+		System.out.println( str instanceof  String);
+	}
+}
