8779df54372d2c489124946eb21b26894b158cdc
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-09-11 10:49:16 +0530

    create calender object

--- String/spliting string/src/Spliting_String.java
+++ String/spliting string/src/Spliting_String.java
@@ -1,18 +1,14 @@
-import java.util.Formattable;
 import java.util.Scanner;
-import java.util.SequencedCollection;
 
 public class Spliting_String {
 	public static void main(String[] args) {
-		System.out.println("spliting  the string ");
+		System.out.println("Splitting the string");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter your string ");
-		String name  = sc.nextLine();
-		String []words = name.split("\\s+");
-//		System.out.println(words[0]);
-		// for each loop in the java
+		System.out.println("Enter your string");
+		String name = sc.nextLine();
+		String[] words = name.split("\\s*,\\s*");
 
-		for (String str:words){
+		for (String str : words) {
 			System.out.println(str);
 		}
 	}
---
+++ calender/src/_1_Example.java
@@ -0,0 +1,12 @@
+import java.util.Calendar;
+
+public class _1_Example {
+	public static void main(String[] args) {
+		Calendar c1 = Calendar.getInstance();
+		int year = c1.get(Calendar.YEAR);
+		int month = c1.get(Calendar.MONTH) + 1;
+		int day = c1.get(Calendar.DAY_OF_MONTH);
+		System.out.println(c1.getTime());
+		System.out.println("Today's date is: " + year + "-" + month + "-" + day);
+	}
+}
