172290d8d749f00af9ad95344409c2d456664e3f
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-09-16 12:35:26 +0530

    iostream file data

--- calender/src/_1_Example.java
+++ calender/src/_1_Example.java
@@ -8,5 +8,6 @@ public class _1_Example {
 		int day = c1.get(Calendar.DAY_OF_MONTH);
 		System.out.println(c1.getTime());
 		System.out.println("Today's date is: " + year + "-" + month + "-" + day);
+		System.out.println(Calendar.SECOND);
 	}
 }
