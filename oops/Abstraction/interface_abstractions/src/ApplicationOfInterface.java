fac04be2a959548d0148eefa6c0b90b1eaec9c63
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-09-21 18:30:51 +0530

    application of the interface

---
+++ oops/Abstraction/interface_abstractions/src/ApplicationOfInterface.java
@@ -0,0 +1,13 @@
+public class ApplicationOfInterface {
+	public static void main(String[] args) {
+		System.out.println("Application of the interface  ");
+		System.out.println("⭐⭐⭐⭐⭐⭐⭐⭐");
+		System.out.println("1.Api design");
+		System.out.println("2. Dependency injection");
+		System.out.println("3. plugin architecture");
+		System.out.println("4. Callbacks");
+		System.out.println("5. Event Handling ");
+		System.out.println("Factory methods implementation");
+		System.out.println("Unit testing");
+	}
+}
