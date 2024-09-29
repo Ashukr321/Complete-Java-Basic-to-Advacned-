f06b651ddb7f9d27a3b8fe42009827a6839d65ef
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-09-29 19:37:29 +0530

    add awt pdf

---
+++ Awt/src/Frame_ex.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class Frame_ex extends Frame {
+	Frame_ex(){
+		this.setTitle("WeL-Come Ashu");
+		setSize(400,300);
+		this.setVisible(true);
+	}
+	public static void main(String[] args) {
+		Frame_ex fm = new Frame_ex();
+	}
+}
