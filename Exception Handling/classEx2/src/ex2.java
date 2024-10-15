d1501a2a68064318897ad6b34d7963eb58335528
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-10-15 10:33:45 +0530

    class word  15/10/24

--- Exception Handling/classEx2/src/ex2.java
+++ Exception Handling/classEx2/src/ex2.java
@@ -3,12 +3,19 @@ import java.util.concurrent.LinkedTransferQueue;
 
 public class ex2 {
 	public static void main(String[] args) {
-		int []arr = new int[4];
+		int[] arr = null;
+		try {
+			arr = new int[-3];
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("we can't give the size of the array -ve");
+		}
+
 		arr[0] = 1;
 		arr[1] = 2;
 		arr[2] = 3;
 		arr[3] = 4;
-		for (int i = 0 ; i<arr.length;i++){
+		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 		}
 		System.out.println();
@@ -16,11 +23,12 @@ public class ex2 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your Idx");
 		idx = sc.nextInt();
-		try{
-		System.out.println(arr[idx]);
+		try {
+			System.out.println(arr[idx]);
 
-		}catch (Exception e){
-			System.out.println("we can't access array element at index:"+ idx);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("we can't access array element at index:" + idx);
 		}
 	}
 }
